e22449ffce530e4d18510a3977e58fdff9172fc3
Alex Bruce <brucealmightyalex@yahoo.com>
2024-12-14 09:52:50 -0700

    Remove hover states for job list and job item

--- src/JobItem.js
+++ src/JobItem.js
@@ -2,18 +2,7 @@ import { useState } from "react";
 import clear from "./assets/clear.svg";
 import "./JobItem.css";
 
-function JobItem({ id, companyName, jobBoard, notes, createdAt, appliedDate, setItemHover, itemHover, deleteApplication }) {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setItemHover(true);
-    setIsHovered(true);
-  }
-
-  const handleMouseLeave = () => {
-    setItemHover(false);
-    setIsHovered(false);
-  }
+function JobItem({ id, companyName, jobBoard, notes, createdAt, appliedDate, deleteApplication }) {
 
   const handleDeletePress = () => {
     const userConfirmed = window.confirm(`Are you sure you want to delete "${companyName}"?`);
@@ -25,22 +14,13 @@ function JobItem({ id, companyName, jobBoard, notes, createdAt, appliedDate, set
   return (
     <div 
     className="job-item" 
-    key={id}
-    onMouseEnter={handleMouseEnter}
-    onMouseLeave={handleMouseLeave}>
-      <div className="job-item-inner-container">
-        <p className="job-item-company">{companyName}</p>
-        <p className="job-item-board">{jobBoard}</p>
-        <p className="job-item-applied-date">{appliedDate}</p>
-      </div>
-      {isHovered ? (
-        <button className="delete-item-button" onClick={handleDeletePress}>
-          <img className="delete-record-icon" src={clear} />
-        </button>
-      ) : itemHover ? (
-        <div className="hover-active-space"></div>
-      ) : null}
+    key={id}>
+    <div className="job-item-inner-container">
+      <p className="job-item-company">{companyName}</p>
+      <p className="job-item-board">{jobBoard}</p>
+      <p className="job-item-applied-date">{appliedDate}</p>
     </div>
+  </div>
   )
 }
 
