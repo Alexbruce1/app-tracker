2b1c10b3fec4e2f84fc01db1c940df02478ec0e6
Alex Bruce <brucealmightyalex@yahoo.com>
2024-12-15 13:06:21 -0700

    Set basic structure for caret to open and close item drawers

--- src/JobItem.js
+++ src/JobItem.js
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import clear from "./assets/clear.svg";
+import caret from "./assets/caret.svg";
 import "./JobItem.css";
 
 function JobItem({ id, companyName, jobBoard, notes, createdAt, appliedDate, deleteApplication }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpansion = () => {
+    setIsExpanded(!isExpanded)
+  }
 
   const handleDeletePress = () => {
     const userConfirmed = window.confirm(`Are you sure you want to delete "${companyName}"?`);
@@ -13,14 +19,22 @@ function JobItem({ id, companyName, jobBoard, notes, createdAt, appliedDate, del
 
   return (
     <div 
-    className="job-item" 
-    key={id}>
-    <div className="job-item-inner-container">
-      <p className="job-item-company">{companyName}</p>
-      <p className="job-item-board">{jobBoard}</p>
-      <p className="job-item-applied-date">{appliedDate}</p>
+      className="job-item" 
+      key={id}>
+      <div className="job-item-top-row">
+        <div className="caret-container">
+          <img 
+            className={isExpanded ? "job-search-icon job-item-caret job-item-caret-expanded" : "job-search-icon job-item-caret"} 
+            src={caret} 
+            onClick={handleExpansion}/>
+        </div>
+        <div className="job-item-inner-container">
+          <p className="job-item-company">{companyName}</p>
+          <p className="job-item-board">{jobBoard}</p>
+          <p className="job-item-applied-date">{appliedDate}</p>
+        </div>
+      </div>
     </div>
-  </div>
   )
 }
 
