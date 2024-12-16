3102ff01e9c910c3c1bd85a48957afb399573744
Alex Bruce <brucealmightyalex@yahoo.com>
2024-12-16 15:04:17 -0700

    Add bottom row to job item

--- src/JobItem.js
+++ src/JobItem.js
@@ -29,11 +29,32 @@ function JobItem({ id, companyName, jobBoard, notes, createdAt, appliedDate, del
             onClick={handleExpansion}/>
         </div>
         <div className="job-item-inner-container">
-          <p className="job-item-company">{companyName}</p>
+          <p className="job-item-company" onClick={handleExpansion}>{companyName}</p>
           <p className="job-item-board">{jobBoard}</p>
           <p className="job-item-applied-date">{appliedDate}</p>
         </div>
       </div>
+      {isExpanded && (
+        <div className="job-item-bottom-row">
+          <div className="job-item-bottom-row-field-container">
+            <div className="job-item-bottom-row-field heard-back">
+              <input type="checkbox" className="heard-back-text-checkbox"/>
+              <p className="heard-back-text">Heard back</p>
+            </div>
+            <select className="status-dropdown">
+              <option>Application Status</option>
+              <option>Haven't heard back</option>
+              <option>Interviewing</option>
+              <option>Offer</option>
+              <option>Declined</option>
+            </select>
+            <p>Notes</p>
+            <button className="delete-item-button" onClick={handleDeletePress}>
+              <img className="delete-record-icon" src={clear} />
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
