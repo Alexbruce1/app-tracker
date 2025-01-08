0a7feb5128643f0cbfd3d0c3439c7f10d670dd7c
Alex Bruce <brucealmightyalex@yahoo.com>
2025-01-08 14:34:25 -0700

    Add status bar to stats section

--- src/Stats.js
+++ src/Stats.js
@@ -8,14 +8,17 @@ function Stats({ jobList }) {
   const [BuiltInCount, setBuiltInCount] = useState(0);
   const [IndeedCount, setIndeedCount] = useState(0);
   const [LinkedInCount, setLinkedInCount] = useState(0);
-
+  const [jobCount, setJobCount] = useState(0);
+  const [statusDeclined, setStatusDeclined] = useState(0);
 
   useEffect(() => {
+    const jobCount = jobList.length;
     const customCoverLetterCount = jobList.filter(job => job.custom_cover_letter).length;
     const heardBackCount = jobList.filter(job => job.heard_back).length;
     const builtin = jobList.filter(job => job.job_board === "BuiltIn").length;
     const indeed = jobList.filter(job => job.job_board === "Indeed").length;
     const linkedin = jobList.filter(job => job.job_board === "LinkedIn").length;
+    const declined = jobList.filter(job => job.status === "Declined").length;
 
     setCustomCoverLetterCount(customCoverLetterCount);
     setHeardBackCount(heardBackCount);
@@ -23,13 +26,55 @@ function Stats({ jobList }) {
     setBuiltInCount(builtin);
     setIndeedCount(indeed);
     setLinkedInCount(linkedin);
+    setJobCount(jobCount);
+    setStatusDeclined(declined);
 
   }, [jobList]);
 
   return (
     <div className="stats-section app-content body-app-content">
       <h2 className="stats-title">Stats</h2>
+      <div className="stats-progress-bar">
+        {haventHeardBackCount > 0 && (
+          <div className="stats-progress-bar-havent-heard-back" style={{ width: `${(haventHeardBackCount / jobCount) * 100}%` }}>
+            {((haventHeardBackCount / jobCount) * 100).toFixed(0)}%
+          </div>
+        )}
+        {statusDeclined > 0 && (
+          <div className="stats-progress-bar-declined" style={{ width: `${(statusDeclined / jobCount) * 100}%` }}>
+            {((statusDeclined / jobCount) * 100).toFixed(0)}%
+          </div>
+        )}
+      </div>
       <div className="stat-container">
+        <p className="stat-title">
+          Jobs Tracked
+        </p> 
+        <p className="stat-count">
+          {jobCount}
+        </p>
+      </div>
+      <div className="stat-container">
+        <p className="stat-title">
+          Haven't Heard Back
+        </p> 
+        <div className="color-indicator color-indicator-havent-heard-back">
+          <p className="stat-count">
+            {haventHeardBackCount}
+          </p>
+        </div>
+      </div>
+      <div className="stat-container">
+        <p className="stat-title">
+          Declined
+        </p> 
+        <div className="color-indicator color-indicator-declined">
+          <p className="stat-count">
+            {statusDeclined}
+          </p>
+        </div>
+      </div>
+      {/* <div className="stat-container">
         <p className="stat-title">
           Custom Cover Letters
         </p> 
@@ -43,14 +88,6 @@ function Stats({ jobList }) {
         </p> 
         <p className="stat-count">
           {heardBackCount}
-        </p>
-      </div>
-      <div className="stat-container">
-        <p className="stat-title">
-          Haven't Heard Back
-        </p> 
-        <p className="stat-count">
-          {haventHeardBackCount}
         </p>
       </div>
       <div className="stat-container">
@@ -76,7 +113,7 @@ function Stats({ jobList }) {
         <p className="stat-count">
           {LinkedInCount}
         </p>
-      </div>
+      </div> */}
     </div>
   );
 }
